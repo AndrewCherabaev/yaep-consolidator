b9e32a80c42d805a4e6ad33f6d60a151ee785821
Andrew Cherabaev <a.cherabaev@php73.ru>
2018-08-19 16:25:31 +0300

    YAEP-CONSOLIDATOR

--- src/templates/yaep.js
+++ src/templates/yaep.js
@@ -1,4 +1,7 @@
 var program = require('yaep-consolidator');
 // if you have your own console commands
-try {require('./app/console')(program);}catch (e){}
+try {require('./app/console')(program);}catch (e){
+	if (process.argv.length == 3 && ~['-h', '--help'].indexOf(process.argv[2]))
+		console.log('\nNOTICE: Please specify index.js in your "app/console" directory');
+}
 program.parse(process.argv);
